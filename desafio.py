a22d3700d03fed50519be7f87f8f27167d768ae1
João Eduardo <joao.eduardo.souza@hotmail.com>
2024-06-01 12:56:16 -0300

    Add files via upload

---
+++ desafio.py
@@ -0,0 +1,43 @@
+
+
+
+
+def funcaoSoma():
+    print("Vamos somar")
+    valor1 = int(input("Digite o primeiro valor: "))
+    valor2 = int(input("Digite o segundo valor:"))
+    
+    resultado = valor1 + valor2
+    
+    print("O resultado da soma dos valores é:", resultado)
+funcaoSoma()
+
+def funcaoSubtracao():
+    print("Vamos subtrair")
+    valor1 = int(input("Digite o primeiro valor: "))
+    valor2 = int(input("Digite o segundo valor:"))
+    
+    resultado = valor1 - valor2
+    
+    print("O resultado da subtração dos valores é:", resultado)
+funcaoSubtracao()
+
+def funcaoMultiplicacao():
+    print("Vamos multiplicar")
+    valor1 = int(input("Digite o primeiro valor: "))
+    valor2 = int(input("Digite o segundo valor:"))
+    
+    resultado = valor1 * valor2
+    
+    print("O resultado da multiplicação dos valores é:", resultado)
+funcaoMultiplicacao()
+    
+def funcaoDivisao():
+    print("Vamos dividir")
+    valor1 = int(input("Digite o primeiro valor: "))
+    valor2 = int(input("Digite o segundo valor:"))
+    
+    resultado = valor1 / valor2
+    
+    print("O resultado da divisão dos valores é:", resultado)
+funcaoDivisao()
